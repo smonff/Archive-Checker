8c2b6b608888d706eeb219a280fa30b34aa4774a
Smonff <smonff@lavabit.com>
2012-08-24 03:32:20 +0200

    BUGGED make it multi-files

--- archive_checker.sh
+++ archive_checker.sh
@@ -3,4 +3,10 @@
 # From http://stackoverflow.com/questions/2001709/how-to-check-if-a-unix-tar-gz-file-is-a-valid-file-without-uncompressing
 # Argument $1 is the path of the file                                                                                      
 # -0 extact to stdout
-tar -xvzf $1 -O > /dev/null
+
+for arg in "$@"
+do
+    echo "$arg"
+    file = "$arg"
+    tar -xvzf $file -O > /dev/null
+done
